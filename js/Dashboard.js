47e9fdd28e3602bd19d8bd35693aafc27e60a453
TalhaFerahPXL <Talha.Ferah@student.pxl.be>
2024-03-08 14:04:37 +0100

    Dashboard

---
+++ js/Dashboard.js
@@ -0,0 +1,11 @@
+
+
+
+
+document.getElementById("BtnReady").addEventListener("click", ()=>{
+    document.querySelector(".GeneratingFrameDone").style.display = "flex"
+
+    document.querySelector(".GeneratingFrame").style.display = "none"
+
+
+})
